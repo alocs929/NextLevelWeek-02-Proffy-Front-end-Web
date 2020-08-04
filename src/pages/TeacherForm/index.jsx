1a77356992756911dad1d1cf39c065d97b856355
Bruno Martins <brunorodrigues929@gmail.com>
2020-08-03 23:40:04 -0300

    Dia 01

---
+++ src/pages/TeacherForm/index.jsx
@@ -0,0 +1,11 @@
+import React from  'react';
+
+function TeacherForm(){
+  return(
+    <>
+      <h1>Forefefwfwfwfwfwfwfwfwfwwfwm</h1>
+    </>
+  );
+}
+
+export default TeacherForm;
---
+++ src/pages/TeacherList/index.jsx
@@ -0,0 +1,12 @@
+import React from  'react';
+
+//// Parei aqui aula 01  time: 1:42:06
+function TeacherList(){
+  return(
+    <>
+      <h1>Teacjfijge</h1>
+    </>
+  );
+}
+
+export default TeacherList;
