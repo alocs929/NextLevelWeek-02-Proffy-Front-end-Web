a352766657f6dac9ff909e444382fe5dca1cb5f7
Bruno Martins <brunorodrigues929@gmail.com>
2020-08-07 01:49:51 -0300

    NLW Day 03 Done

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -5,10 +5,10 @@ import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
 
 import './styles.css';
-import { title } from 'process';
 
 interface PageHeaderProps{
   title: string;
+  description?: string;
 }
 
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
@@ -23,6 +23,7 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
 
       <div className="header-content">
         <strong>{props.title}</strong>
+        {props.description && <p>{props.description}</p>}
         {props.children}
       </div>
       
--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,33 +3,57 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-const TeacherItem = () =>{
+export interface Teacher {
+  id: number,
+  avatar: string,
+  bio: string,
+  cost: number,
+  name: string,
+  subject: string,
+  whatsapp: string
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) =>{
+
+  function createNewConnection (){
+    api.post('/connections', {
+      user_id: teacher.id,
+    });
+  }
   return(
     <article className="teacher-item">
       <header>
-        <img src="https://avatars3.githubusercontent.com/u/55994601?s=460&u=e36d24e87d392b6a37d3546a62b0a919ec64f8d5&v=4" alt="Bruno Martins"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>Bruno Martins</strong>
-          <span>Quimica</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
       <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br/> <br/>
-        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+        {teacher.bio}
       </p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a 
+          target="_blank"
+          onClick={createNewConnection} 
+          href={`http://wa.me/55${teacher.whatsapp}`} 
+          type="button"
+        >
           <img src={whatsappIcon} alt="Whatsapp"/>
           Entrar em contato.
-        </button>
+        </a>
       </footer>
     </article>
   );
